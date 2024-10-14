0abb8fded0ad4e1d4cbab40d032605a17aa2b8d6
HeronErin <HeronErin@protonmail.com>
2024-10-14 13:10:29 -0400

    init

---
+++ fix.py
@@ -0,0 +1,14 @@
+import os
+
+
+for (root,_,files) in os.walk("."):
+	mds = list(filter(lambda x: x.endswith(".md"), files))
+	for fn in mds:
+		f = open(os.path.join(root, fn), "r")
+		mcode = f.read()
+		f.close()
+
+		f = open(os.path.join(root, fn), "w")
+		f.write(mcode.replace("$$", "\n$$\n"))
+		f.close()
+
